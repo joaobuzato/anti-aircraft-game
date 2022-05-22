1f1ec9f7288a82ff192866c677f93de6cee28e58
Buzato <joaobuzato@gmail.com>
2022-05-22 07:44:24 -0300

    adequating to OOP

--- cannon.py
+++ cannon.py
@@ -5,19 +5,37 @@ TURN_DEGREES = 45
 
 class Cannon(Turtle):
 
-    def __init__(self):
+    def __init__(self, screen):
         super().__init__()
         self.penup()
         self.setheading(90)
+        self.screen = screen
         self.goto(STARTING_POS)
+        self.cannon_img = "img/cannon.gif"
+        self.cannon_img_left_45 = "img/cannon-45.gif"
+        self.cannon_img_right_45 = "img/cannon45.gif"
+
+        self.screen.register_shape(self.cannon_img)
+        self.screen.register_shape(self.cannon_img_left_45)
+        self.screen.register_shape(self.cannon_img_right_45)
+
+        self.shape(self.cannon_img)
 
     def turn_right(self):
         if self.heading() < 135:
             self.setheading(self.heading()+TURN_DEGREES)
+            if self.heading() == 90:
+                self.shape(self.cannon_img)
+            else:
+                self.shape(self.cannon_img_right_45)
 
     def turn_left(self):
         if self.heading() > 45:
             self.setheading(self.heading()-TURN_DEGREES)
+            if self.heading() == 90:
+                self.shape(self.cannon_img)
+            else:
+                self.shape(self.cannon_img_left_45)
 
     def shoot(self):
         # TODO
--- main.py
+++ main.py
@@ -8,38 +8,15 @@ screen.setup(width=410, height=310)
 screen.title("Anti-Aircraft")
 bg_img = "img/bg.gif"
 screen.bgpic(bg_img)
-cannon_img = "img/cannon.gif"
-cannon_img_left_45 = "img/cannon-45.gif"
-cannon_img_right_45 = "img/cannon45.gif"
-
-screen.register_shape(cannon_img)
-screen.register_shape(cannon_img_left_45)
-screen.register_shape(cannon_img_right_45)
 
 
-cannon = Cannon()
-cannon.shape(cannon_img)
 
-
-def cannon_turn_left():
-    cannon.turn_left()
-    if cannon.heading() == 90:
-        cannon.shape(cannon_img)
-    else:
-        cannon.shape(cannon_img_left_45)
-
-
-def cannon_turn_right():
-    cannon.turn_right()
-    if cannon.heading() == 90:
-        cannon.shape(cannon_img)
-    else:
-        cannon.shape(cannon_img_right_45)
+cannon = Cannon(screen)
 
 
 screen.listen()
-screen.onkey(key="a", fun=cannon_turn_left)
-screen.onkey(key="d", fun=cannon_turn_right)
+screen.onkey(key="a", fun=cannon.turn_left)
+screen.onkey(key="d", fun=cannon.turn_right)
 screen.onkey(key="k", fun=cannon.shoot)
 
 screen.tracer(0)
